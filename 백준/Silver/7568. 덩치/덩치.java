725c2e01ebef392d817cf29ae5221d925d4ee530
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2024-09-15 17:48:45 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        int [][] arr= new int[n][2];
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken()); // 몸무게
+            arr[i][1] = Integer.parseInt(st.nextToken()); //키
+        }
+
+        for(int i = 0; i < n; i++) {
+            int rank=1;
+            for(int j = 0; j < n; j++) {
+                if(i==j) continue;
+                if(arr[i][0]<arr[j][0]&&arr[i][1]<arr[j][1]) rank++;
+
+            }
+            sb.append(rank).append(" ");
+        }
+
+        System.out.println(sb);
+
+    }
+}
