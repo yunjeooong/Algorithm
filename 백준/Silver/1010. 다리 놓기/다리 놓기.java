5eea7612aa6594dfe74bbdc2bf229ff02c1576da
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2024-09-19 23:43:32 +0900

    [Silver V] Title: 다리 놓기, Time: 112 ms, Memory: 16244 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,37 +1,46 @@
-import java.io.*;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int T = Integer.parseInt(br.readLine()); // 테스트 개수
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 
+        StringBuilder sb = new StringBuilder();
 
+       
         for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-
-            bw.write(getBridge(M, N).toString() + "\n");
+            String[] input = br.readLine().split(" ");
+            int N = Integer.parseInt(input[0]); 
+            int M = Integer.parseInt(input[1]); 
+            sb.append(combi(M, N)).append('\n'); 
         }
 
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 
-    // 조합을 계산하는 함수 (BigInteger 사용)
-    static BigInteger getBridge(int M, int N) {
-        BigInteger result = BigInteger.ONE; // 0과 1 팩토리얼은 1이기 때문에 1부터 시작
-
-        // mCn 계산
-        for (int i = 0; i < N; i++) {
-            result = result.multiply(BigInteger.valueOf(M - i));
-            result = result.divide(BigInteger.valueOf(i + 1));
+    // 조합
+    static int combi(int M, int N) {
+        if (N == 0 || M == N) {
+            return 1; 
         }
 
-        return result;
+        int[][] dp = new int[31][31];
+
+        // 초기화
+        for (int i = 0; i <= M; i++) {
+            dp[i][0] = 1; // nC0 = 1
+            dp[i][i] = 1; // nCn = 1
+        }
+
+        // 채우기
+        for (int i = 1; i <= M; i++) {
+            for (int j = 1; j <= N && j < i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        return dp[M][N];
     }
 }
